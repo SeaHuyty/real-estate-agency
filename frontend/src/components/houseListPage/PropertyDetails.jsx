884a5ac060e1f25f5e580e21f687acd8c5fe93c8
SeaHuyty <seahuyty4444@gmail.com>
2025-06-21 10:59:38 +0700

    Delete: remove code redundancy
    Co-authored-by: Long Chhunhour <longchhunhour74@gmail.com>

--- frontend/src/components/houseListPage/PropertyDetails.jsx
+++ frontend/src/components/houseListPage/PropertyDetails.jsx
@@ -230,42 +230,7 @@ const PropertyDetails = () => {
                         <h1 className='font-semibold text-[22px] mb-4'>Similar Properties</h1>
                         <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5'>
                             {similarProperties.map(property => (
-                                <PropertyCard key={property.id} property={property} className='flex flex-col gap-3'>
-                                    <div>
-                                        {property.property_thumbnail ? (
-                                            <img src={property.property_thumbnail} alt="property.title" className='w-full h-[300px] object-cover object-center' />
-                                        ) : (
-                                            <div className='w-full h-48 bg-gray-200 rounded flex items-center justify-center' >
-                                                <span>No Image</span>
-                                            </div>
-                                        )}
-                                    </div>
-
-                                    <div className="flex flex-col justify-between gap-[10px]">
-                                        <div className="flex justify-between">
-                                            <h2 className="text-[20px] font-semibold">{property.title}</h2>
-                                            <p className='text-[20px] font-semibold text-green-600'>$ {Number(property.price).toLocaleString()}</p>
-                                        </div>
-                                        <p className='text-[14px] flex items-center gap-[10px] text-gray-500'>
-                                            <img className='w-[20px] h-[20px]' src="/pin.png" alt="" />
-                                            <span>{property.address}, {property.city}</span>
-                                        </p>
-                                        <div className="flex gap-[20px] text-[14px]">
-                                            <div className="flex items-center gap-[5px] bg-white">
-                                                <img src="/bed.png" alt="" className='w-[20px] h-[20px]' />
-                                                <span>{property.bedrooms} bedroom</span>
-                                            </div>
-                                            <div className="flex items-center gap-[5px] bg-white">
-                                                <img src="/bath.png" alt="" className='w-[20px] h-[20px]' />
-                                                <span>{property.bathrooms} bathroom</span>
-                                            </div>
-                                            <div className="flex items-center gap-[5px] bg-white">
-                                                <img src="/size.png" alt="" className='w-[20px] h-[20px]' />
-                                                <span>{property.size} m²</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </PropertyCard>
+                                <PropertyCard key={property.id} property={property} className='flex flex-col gap-3' />
                             ))}
                         </div>
                     </div>
